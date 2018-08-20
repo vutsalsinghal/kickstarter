adb4c4b8683cd42f34e52ef203b433cd1a415553
Vutsal Singhal <vutsalsinghal@gmail.com>
2018-08-20 14:59:28 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Modal } from 'semantic-ui-react';
+import CampaignNew from '../pages/campaigns/new';
 
 export default () => {
-	return (
-	    <Menu style={{ marginTop:'10px' }}>
-	      	<Menu.Item>CrowdCoin</Menu.Item>
+    return (
+        <Menu style={{ marginTop:'10px' }}>
+            <Menu.Item>CrowdCoin</Menu.Item>
 
-	       	<Menu.Menu position="right">
-	      		<Menu.Item>Campaigns</Menu.Item>
-
-  	    		<Menu.Item>+</Menu.Item>
-	      	</Menu.Menu>
-	    </Menu>
-  	);
+            <Menu.Menu position="right">
+                <Modal trigger={<Menu.Item>+</Menu.Item>}>
+                    <Modal.Header>Create New Campaign</Modal.Header>
+                    <Modal.Content>
+                        <CampaignNew />
+                    </Modal.Content>
+                </Modal>
+            </Menu.Menu>
+        </Menu>
+    );
 };
